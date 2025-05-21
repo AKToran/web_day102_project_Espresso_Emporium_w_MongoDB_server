80c22faeb2bab9e720a60ce0591879f2dcdc58c5
AKToran <aktoran60@gmail.com>
2025-05-21 12:45:03 +0600

    mongodb server setup

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cjjjauk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } 
+  finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.use(cors());
