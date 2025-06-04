b9137fa1ec7b785e360a5278e69b397095f41b3f
AKToran <aktoran60@gmail.com>
2025-06-05 05:13:15 +0600

    fix:vercel deployment issue

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+
+app.use(cors());
+app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cjjjauk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -17,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     //create or access db:
     const database = client.db('coffeeDB');
@@ -95,8 +99,8 @@ async function run() {
     })
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } 
   finally {
     // don't want to close the server after just one ping.
@@ -106,8 +110,6 @@ async function run() {
 run().catch(console.dir);
 
 
-app.use(cors());
-app.use(express.json());
 
 app.get('/', (req, res)=>{
   res.send("Coffee is brewing....");
